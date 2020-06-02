3e6885fc193fa35dbff451f2f6b8c80fca8e21e8
Daniil Popov <lalabuy9948@gmail.com>
2020-06-02 14:35:00 +0200

    [TPG] feat: Setup webhook server.

--- WebhookServer.rb
+++ WebhookServer.rb
@@ -3,8 +3,22 @@
 
 require 'sinatra'
 require 'json'
+require 'logger'
+
+log = Logger.new('pipeline.log')
+
+def verify_signature(payload_body)
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+    return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+end
 
 post '/payload' do
-  push = JSON.parse(request.body.read)
-  puts "I got some JSON: #{push.inspect}"
+    payload_body = request.body.read
+    verify_signature(payload_body)
+
+    push = JSON.parse(payload_body)
+    log.debug "webhook recieved... commit [#{push["head_commit"]["id"]}]"
+    if push["ref"] == "refs/heads/master"
+        log.debug "starting pipeline..."
+    end
 end
