d1d0898fd742d37b4d61ec4ded82394141061855
Daniil Popov <Lalabuy9948@gmail.com>
2020-06-02 21:28:28 +0200

    Feature/docker (#1)

---
+++ infra/pipeline.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+cd ..
+echo "pulling repository on master..."
+git checkout master
+git pull
+cd backend
+echo "building backend image..."
+docker build -t typing-backend -f Dockerfile .
+echo "getting go dependencies..."
+docker run -v $(pwd)/:/go/src/builder typing-backend go get
+echo "building go application..."
+docker run -v $(pwd)/:/go/src/builder typing-backend go build
+echo "killing previous backend..."
+kill -9 `cat run.pid`
+echo "starting new backend..."
+nohup ./backend  >/var/log/go-server.log 2>&1 & echo $! > run.pid
+echo "going to frontend folder..."
+cd ..
+cd frontend
+echo "building frontend image..."
+docker build -t typing-frontend -f Dockerfile .
+echo "getting npm dependencies..."
+docker run -v $(pwd)/:/frontend typing-frontend npm install
+echo "building react application..."
+docker run -v $(pwd)/:/frontend typing-frontend npm run build
+echo "restarting nginx..."
+service nginx restart
